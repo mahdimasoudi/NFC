fdedaa8c2ce3e35c3f76623f275f3fc898092f16
Mahdi Masoudi <masoudi@bmsd.ir>
2024-06-30 14:55:26 +0330

    Creat NFC

--- NFC/NFC.Android/MainActivity.cs
+++ NFC/NFC.Android/MainActivity.cs
@@ -1,13 +1,17 @@
-﻿using System;
-
+﻿
 using Android.App;
 using Android.Content.PM;
 using Android.Runtime;
 using Android.OS;
+using Android.Nfc;
+using Android.Content;
+using Plugin.NFC;
 
 namespace NFC.Droid
 {
+    
     [Activity(Label = "NFC", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
+    [IntentFilter(new[] { NfcAdapter.ActionNdefDiscovered }, Categories = new[] { Intent.CategoryDefault }, DataMimeType = MainPage.MIME_TYPE)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
@@ -16,7 +20,19 @@ namespace NFC.Droid
 
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
+            CrossNFC.Init(this);
             LoadApplication(new App());
+        }
+        protected override void OnResume()
+        {
+            base.OnResume();
+            CrossNFC.OnResume();
+        }
+
+        protected override void OnNewIntent(Intent intent)
+        {
+            base.OnNewIntent(intent);
+            CrossNFC.OnNewIntent(intent);
         }
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
         {
